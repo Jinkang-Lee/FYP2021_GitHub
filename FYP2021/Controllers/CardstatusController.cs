0e8a8ade3392a7a967cd362ca1658cdefbbbcc14
SOON YI YANG RP <19024868@myrp.edu.sg>
2021-05-12 22:47:52 +0800

    Update card status draft

--- FYP2021/Controllers/CardstatusController.cs
+++ FYP2021/Controllers/CardstatusController.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Data;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FYP2021.Controllers
 {
-
+    public class CardstatusController : Controller
+    {
+        public IActionResult ListCard()
+         {
+            return View("ListCard");
+        }
+    }
 }
 //Yiyang testing
