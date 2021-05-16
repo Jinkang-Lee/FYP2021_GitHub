eca32df68d1318cda9d777c7edf2c6271bb75940
Jinkang-Lee <68258103+Jinkang-Lee@users.noreply.github.com>
2021-05-17 00:39:49 +0800

    Almost done with admin login, but cannot verify admin email and password

--- FYP2021/Controllers/AdminAccountController.cs
+++ FYP2021/Controllers/AdminAccountController.cs
@@ -1,116 +1,116 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//// FOR ACCOUNT PURPOSES
-//using Microsoft.AspNetCore.Authentication;
-//using Microsoft.AspNetCore.Authentication.Cookies;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-//using System.Security.Claims;
+// FOR ACCOUNT PURPOSES
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Data;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
-//// ADD using MODEL
+// ADD using MODEL
+using FYP2021.Models;
 
-//namespace FYP2021.Controllers
+namespace FYP2021.Controllers
 
-//{
-//    [Authorize(AuthenticationSchemes = "Admin")]
-//    public class AdminAccountController : Controller
-//    {
-//        private const string AUTHSCHEME = "Admin";
+{
+    [Authorize(AuthenticationSchemes = "AdminAccount")]
+    public class AdminAccountController : Controller
+    {
+        private const string AUTHSCHEME = "AdminAccount";
 
-//        // Uncomment only AFTER DATABASE IS DONE!!!
-//        //private const string LOGIN_SQL =
-//        //@"SELECT * FROM <INSERT ADMIN USER HERE> 
-//        // WHERE <INSERT Admin Email> = '{0}' 
-//        // AND <INSERT Admin Password> = HASHBYTES('SHA1', '{1}')";
-
-
-//        private const string ROLE_COL = "Role";
-//        private const string NAME_COL = "Name";
-
-//        private const string REDIRECT_CNTR = "Admin";
-//        private const string REDIRECT_ACTN = "Index";
-
-//        private const string LOGIN_VIEW = "Login";
-
-//        [AllowAnonymous]
-//        public IActionResult Login(string returnUrl = null)
-//        {
-//            TempData["ReturnUrl"] = returnUrl;
-//            return View(LOGIN_VIEW);
-//        }
+        private const string LOGIN_SQL =
+        @"SELECT * FROM Admin
+         WHERE admin_email = '{0}' 
+        AND admin_password = HASHBYTES('SHA1', '{1}')";
 
 
+        private const string ROLE_COL = "Role";
+        private const string NAME_COL = "Name";
 
-//        // FOR LOGIN
-//        [AllowAnonymous]
-//        [HttpPost]
-//        public IActionResult Login(LoginUser user)
-//        {
-//            if (!AuthenticateUser(user.UserId, user.Password, out ClaimsPrincipal principal))
-//            {
-//                ViewData["Message"] = "Incorrect Email or Password";
-//                ViewData["MsgType"] = "warning";
-//                return View(LOGIN_VIEW);
-//            }
+        private const string REDIRECT_CNTR = "Admin";
+        private const string REDIRECT_ACTN = "Index";
 
-//            //Sign in using the assigned AUTH SCHEME
-//            else
-//            {
-//                HttpContext.SignInAsync(
-//                   AUTHSCHEME,
-//                   principal,
-//               new AuthenticationProperties
-//               {
-//                   IsPersistent = false
-//               });
+        private const string LOGIN_VIEW = "Login";
 
-//                return RedirectToAction(REDIRECT_ACTN, REDIRECT_CNTR);
-//            }
-//        }
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            TempData["ReturnUrl"] = returnUrl;
+            return View(LOGIN_VIEW);
+        }
 
 
-//        // FOR LOGOUT
-//        [Authorize]
-//        public IActionResult Logoff(string returnUrl = null)
-//        {
-//            HttpContext.SignOutAsync(AUTHSCHEME);
-//            if (Url.IsLocalUrl(returnUrl))
-//                return Redirect(returnUrl);
-//            return RedirectToAction(REDIRECT_ACTN, REDIRECT_CNTR);
-//        }
 
-//        [AllowAnonymous]
-//        public IActionResult Forbidden()
-//        {
-//            return View();
-//        }
+        // FOR LOGIN
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(LoginUser user)
+        {
+            if (!AuthenticateUser(user.UserId, user.Password, out ClaimsPrincipal principal))
+            {
+                ViewData["Message"] = "Incorrect Email or Password";
+                ViewData["MsgType"] = "warning";
+                return View(LOGIN_VIEW);
+            }
+
+            //Sign in using the assigned AUTH SCHEME
+            else
+            {
+                HttpContext.SignInAsync(
+                   AUTHSCHEME,
+                   principal,
+               new AuthenticationProperties
+               {
+                   IsPersistent = false
+               });
+
+                return RedirectToAction(REDIRECT_ACTN, REDIRECT_CNTR);
+            }
+        }
 
 
-//        // FOR AUTHENTICATING USERS
-//        private bool AuthenticateUser(string uid, string pw, out ClaimsPrincipal principal)
-//        {
-//            principal = null;
+        // FOR LOGOUT
+        [Authorize]
+        public IActionResult Logoff(string returnUrl = null)
+        {
+            HttpContext.SignOutAsync(AUTHSCHEME);
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            return RedirectToAction(REDIRECT_ACTN, REDIRECT_CNTR);
+        }
 
-//            DataTable ds = DBUtl.GetTable(LOGIN_SQL, uid, pw);
-//            if (ds.Rows.Count == 1)
-//            {
-//                principal =
-//                   new ClaimsPrincipal(
-//                      new ClaimsIdentity(
-//                         new Claim[] {
-//                        new Claim(ClaimTypes.NameIdentifier, ds.Rows[0]["Id"].ToString()),
-//                        new Claim(ClaimTypes.Name, ds.Rows[0][NAME_COL].ToString()),
-//                        new Claim(ClaimTypes.Role, ds.Rows[0][ROLE_COL].ToString())
-//                         }, "Basic"
-//                      )
-//                   );
-//                return true;
-//            }
-//            return false;
-//        }
-//    }
-//}
+        [AllowAnonymous]
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+
+
+        // FOR AUTHENTICATING USERS
+        private bool AuthenticateUser(string uid, string pw, out ClaimsPrincipal principal)
+        {
+            principal = null;
+
+            DataTable ds = DBUtl.GetTable(LOGIN_SQL, uid, pw);
+            if (ds.Rows.Count == 1)
+            {
+                principal =
+                   new ClaimsPrincipal(
+                      new ClaimsIdentity(
+                         new Claim[] {
+                        new Claim(ClaimTypes.NameIdentifier, ds.Rows[0]["Id"].ToString()),
+                        new Claim(ClaimTypes.Name, ds.Rows[0][NAME_COL].ToString()),
+                        new Claim(ClaimTypes.Role, ds.Rows[0][ROLE_COL].ToString())
+                         }, "Basic"
+                      )
+                   );
+                return true;
+            }
+            return false;
+        }
+    }
+}
