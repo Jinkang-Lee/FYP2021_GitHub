0ac7a2d7f7e759cac5ffe6a4c7ace81393fa017e
Md Syakir Rahmat <Pein-shark@hotmail.com>
2021-05-19 12:42:55 +0800

    syakir push - added own controller and view for testing webpage/css

---
+++ FYP2021/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FYP2021.Controllers
+{
+    public class ReportController : Controller
+    {
+        // View to the generate report in Admin folder
+        public IActionResult GenerateReport()
+        {
+            return View();
+        }
+    }
+}
