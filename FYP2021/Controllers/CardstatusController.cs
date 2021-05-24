870ce3fdcaf474766c7f25b03f481b301424bd2c
SOON YI YANG RP <19024868@myrp.edu.sg>
2021-05-24 22:44:34 +0800

    Able to get data from the database to table, sorting not working

--- FYP2021/Controllers/CardstatusController.cs
+++ FYP2021/Controllers/CardstatusController.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FYP2021.Models;
+using Microsoft.AspNetCore.Http;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Dynamic;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
 
 namespace FYP2021.Controllers
 {
@@ -10,7 +19,9 @@ namespace FYP2021.Controllers
     {
         public IActionResult ListCard()
         {
-            return View();
+            string sql = "SELECT * FROM Student";
+            DataTable dt = DBUtl.GetTable(sql);
+            return View(dt.Rows);
         }
     }
 }
