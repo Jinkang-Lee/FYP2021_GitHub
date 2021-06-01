d6a0de1208a78a078dc41ad2f2b7f6eeebfe1d98
Md Syakir Rahmat <Pein-shark@hotmail.com>
2021-06-01 21:57:10 +0800

    created announcement controller and views

---
+++ FYP2021/Controllers/AnnouncementController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FYP2021.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
