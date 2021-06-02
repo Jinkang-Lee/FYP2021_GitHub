0358fe067e4c8c1f6b0927847fc586fe5f93912e
SOON YI YANG RP <19024868@myrp.edu.sg>
2021-06-02 10:38:21 +0800

    updated query card status

--- FYP2021/Controllers/QueryController.cs
+++ FYP2021/Controllers/QueryController.cs
@@ -3,13 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FYP2021.Models;
+using System.Data;
 
 namespace FYP2021.Controllers
 {
-    public class Query : Controller
+    public class QueryController : Controller
     {
         public IActionResult QueryCardStatus()
         {
+            string sql = "SELECT * FROM Student WHERE student_email = '{0}'";
+            DataTable dt = DBUtl.GetTable(sql);
             return View();
         }
 
