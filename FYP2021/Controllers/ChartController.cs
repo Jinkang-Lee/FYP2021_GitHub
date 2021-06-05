86ed98ac510867995f42286d333a2ee6b4f8d5d1
Md Syakir Rahmat <Pein-shark@hotmail.com>
2021-06-05 20:36:49 +0800

    added views, models, controllers for chartjs

---
+++ FYP2021/Controllers/ChartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FYP2021.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Pie()
+        {
+            ViewData["Chart"] = "pie";
+            ViewData["Title"] = "Report Summary";
+            ViewData["ShowLegend"] = "true";
+            return View("Chart");
+        }
+    }
+}
---
+++ FYP2021/Models/ReportChart.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.Collections.Generic;
+
+namespace FYP2021.Models
+{
+    public class ReportChart
+    {
+        public string ReportChartGUID { get; set; }
+
+    }
+}
